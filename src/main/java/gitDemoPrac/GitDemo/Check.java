3370d461f5afca87e815b327c0688ff550479e57
rakesh-kumar85 <rakesh.kumar.rkk@gmail.com>
2021-02-07 01:08:15 +0530

    This is from Rakesh asian

--- src/main/java/gitDemoPrac/GitDemo/Check.java
+++ src/main/java/gitDemoPrac/GitDemo/Check.java
@@ -5,6 +5,10 @@ public class Check {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hi");
+     System.out.println("Hi second line");
+     System.out.println("Hi thired line");
+     System.out.println("Hi fourth line");
+     
 	}
 
 }
